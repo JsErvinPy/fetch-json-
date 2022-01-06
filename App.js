5a9ac8032c0d4bf261eb98aab29ba15c573190e9
JsErvinPy <95141419+JsErvinPy@users.noreply.github.com>
2022-01-06 21:03:55 +0300

    fetch json react
    fetch json react

---
+++ App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import './App.css';
+class App extends React.Component {
+
+	// Constructor
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: [],
+			DataisLoaded: false
+		};
+	}
+
+	// ComponentDidMount is used to
+	// execute the code
+	componentDidMount() {
+		fetch(
+"https://enjirtmnv.github.io/react-redux-toolkit-app-like/")
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true
+				});
+			})
+	}
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Waiting json fetch </h1> </div> ;
+
+		return (
+		<div className = "App">
+			<h1> Fetch data from an api in react </h1> {
+				items.map((item) => (
+				<ol key = { item.mal_id } >
+					User_Name: { item.title },
+					Full_Name: { item.url },
+					User_Email: { item.image_url }
+					</ol>
+				))
+			}
+		</div>
+	);
+}
+}
+export default App;
